c98be93532d170d88daec125112b7608f8f2af42
Oxojo <oxojo9x@gmail.com>
2025-08-17 22:55:39 +0900

    fix: template.cpp の修正

--- template.cpp
+++ template.cpp
@@ -3,28 +3,12 @@ using namespace std;
 using ll = long long;
 using ld = long double;
 using pll = pair<ll, ll>;
-using int128 = __int128;
-using State = string::const_iterator;
-class ParseError {};
-#define rep(i, n) for(ll i = 0; i < (n); i++)
+using vl = vector<ll>;
+#define rep(i, r) for(ll i = 0; i < (r); i++)
 #define reps(i, l, r) for(ll i = (l); i < (r); i++)
 #define all(a) (a).begin(), (a).end()
-#define rall(a) (a).rbegin(), (a).rend()
-#define endl "\n";
-const ll INF = LLONG_MAX / 4;
-const ld inf = numeric_limits<long double>::max() / (ld)4;
-const ll mod1 = 1000000007;
-const ll mod2 = 998244353;
-const ld pi = 3.1415926535897;
-ll dx[8] = {1, 1, 0, -1, -1, -1, 0, 1};
-ll dy[8] = {0, -1, -1, -1, 0, 1, 1, 1};
+#define sz(a) (ll)(a).size()
 template <typename T>
-bool chmax(T &a, const T& b) {
-  if (a < b) { a = b; return true; }
-  return false;
-}
+bool chmax(T &a, const T& b) { return a < b ? a = b, true : false; }
 template <typename T>
-bool chmin(T &a, const T& b) {
-  if (a > b) { a = b; return true; }
-  return false;
-}
+bool chmin(T &a, const T& b) { return a > b ? a = b, true : false; }
